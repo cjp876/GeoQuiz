8355237a42ff52da7b67a96ae28aab4cacb6c04a
cjpurcell <cjp876@live.missouristate.edu>
2023-12-10 17:18:06 -0600

    GeoQuiz App

--- app/src/main/java/com/purcell/msu/geoquizapp/MainActivity.kt
+++ app/src/main/java/com/purcell/msu/geoquizapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.purcell.msu.geoquizapp
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +18,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        trueButton.setOnClickListener{
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+      /*  trueButton.setOnClickListener{
             Toast.makeText(
                 this,
                 R.string.true_button,
@@ -31,6 +36,32 @@ class MainActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT)
                 .show()
         }
+        */
+
+        trueButton.setOnClickListener{
+            val snackBar = Snackbar.make(
+                it,
+                "Correct",
+                Snackbar.LENGTH_LONG
+
+            )
+            snackBar.show()
+        }
+
+        falseButton.setOnClickListener{
+            val snackBar = Snackbar.make(
+                it,
+                "Incorrect",
+                Snackbar.LENGTH_LONG
+
+            )
+            snackBar.setTextColor(Color.BLACK)
+            snackBar.setBackgroundTint(Color.RED)
+            snackBar.show()
+        }
+
+
+
     }
 
 }
